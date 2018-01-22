a1ab3dd93d254b0e6c423a7796c4f94935448a9c
Unknown <ryshaunwillis@gmail.com>
2018-01-21 21:11:19 -0600

    collective of three weeks work on firebase
    Working on Facebook login in page and generating  a firebase key for user when logging in using facebook
    key is created but still trying to figure out getting data to show on online console

--- app/src/main/java/com/rap/rhythmandpoetry/FacebookLogin.java
+++ app/src/main/java/com/rap/rhythmandpoetry/FacebookLogin.java
@@ -1,20 +1,37 @@
 package com.rap.rhythmandpoetry;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.facebook.*;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.rap.rhythmandpoetry.updateProfile;
+
+import static android.content.ContentValues.TAG;
 
 public class FacebookLogin extends Activity
 {
@@ -23,6 +40,16 @@ public class FacebookLogin extends Activity
     LoginButton login_button;
     String email,name,first_name,last_name;
     private TextView info;
+    private FirebaseAuth mAuth;
+    private static updateProfile userInfo = new updateProfile();
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -30,9 +57,7 @@ public class FacebookLogin extends Activity
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(this.getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-
-
-
+        mAuth = FirebaseAuth.getInstance();
 
         setContentView(R.layout.facebook_login);
         info = (TextView)findViewById(R.id.info);
@@ -48,9 +73,11 @@ public class FacebookLogin extends Activity
             public void onSuccess(LoginResult loginResult)
             {
                 login_button.setVisibility(View.GONE);
+                handleFacebookAccessToken(loginResult.getAccessToken());
 
                 GraphRequest graphRequest   =   GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback()
                 {
+
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response)
                     {
@@ -73,17 +100,18 @@ public class FacebookLogin extends Activity
                         {
                             e.printStackTrace();
                         }
-                    }
-                });
+                        }
+                    });
 
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,first_name,last_name,email");
-                graphRequest.setParameters(parameters);
-                graphRequest.executeAsync();
+                    Bundle parameters = new Bundle();
+                    parameters.putString("fields", "id,name,first_name,last_name,email");
+                    graphRequest.setParameters(parameters);
+                    graphRequest.executeAsync();
 
-                Intent i = new Intent(FacebookLogin.this, RapRoot.class);
-                startActivity(i);
+                    Intent i = new Intent(FacebookLogin.this, RapRoot.class);
+                    startActivity(i);
             }
+
 
             @Override
             public void onCancel()
@@ -97,6 +125,34 @@ public class FacebookLogin extends Activity
                 info.setText("Login attempt failed.");
             }
         });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        final AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Iterator<String> myVeryOwnIterator = userInfo.userData.keySet().iterator();
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            User profile_info = new User(userInfo.getElementByIndex(userInfo.userData,0).toString(),
+                                    userInfo.getElementByIndex(userInfo.userData,1).toString(),
+                                    userInfo.getElementByIndex(userInfo.userData,2).toString());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(FacebookLogin.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
     }
 
 
--- app/src/main/java/com/rap/rhythmandpoetry/updateProfile.java
+++ app/src/main/java/com/rap/rhythmandpoetry/updateProfile.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.rap.rhythmandpoetry.User;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by rysha on 12/8/2017.
@@ -14,21 +22,46 @@ import android.widget.EditText;
 public class updateProfile extends Activity {
 
     private EditText userName, bio, imageurl;
-    private Button updateButton;
+    private Button updateButton, submitButton;
     private static int GET_FROM_GALLERY = 1;
+    //private DatabaseReference mDatabase;
+    LinkedHashMap<String, String> userData = new LinkedHashMap<String, String>();
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("user_info");
+    String key = mDatabase.push().getKey();
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_update_profile);
         Button updateButton = (Button) findViewById(R.id.update);
-        EditText userName = (EditText) findViewById(R.id.user_name);
-        EditText bio = (EditText) findViewById(R.id.bio);
-        EditText imageUrl = (EditText) findViewById(R.id.imageUrl);
+        Button submitButton = (Button) findViewById(R.id.submit);
+        final EditText userName = (EditText) findViewById(R.id.user_name);
+        final EditText bio = (EditText) findViewById(R.id.bio);
+        final EditText imageUrl = (EditText) findViewById(R.id.imageUrl);
+        //mDatabase = FirebaseDatabase.getInstance().getReference();
 
         updateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
             }
-    });}
+    });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                userData.put("User name",userName.toString());
+                userData.put("Bio",bio.toString());
+                userData.put("image", imageUrl.toString());
+
+                mDatabase.setValue(userData);
+
+                Intent i = new Intent(updateProfile.this, RapRoot.class);
+                startActivity(i);
+            }
+
+        });
+    }
+    public Object getElementByIndex(LinkedHashMap map, int index){
+        return map.get( (map.keySet().toArray())[ index ] );
+    }
 }
