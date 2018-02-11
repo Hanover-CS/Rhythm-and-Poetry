f5e5fc9ebc5278f273ef6cdfa99eaa19451d757b
Unknown <ryshaunwillis@gmail.com>
2018-02-11 14:46:17 -0600

    Updating Vice president name
    updated file name and info

--- app/src/main/java/com/rap/rhythmandpoetry/RapRoot.java
+++ app/src/main/java/com/rap/rhythmandpoetry/RapRoot.java
@@ -1,11 +1,9 @@
 package com.rap.rhythmandpoetry;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.facebook.CallbackManager;
 
 
 public class RapRoot extends AppCompatActivity
@@ -102,7 +99,7 @@ public class RapRoot extends AppCompatActivity
         } else if (id == R.id.nav_second_layout) {
             fragmentManager.beginTransaction()
                     .replace(R.id.content_frame
-                            , new SecondFragment())
+                            , new VicePresidentFragment())
                     .commit();
         } else if (id == R.id.nav_third_layout) {
             fragmentManager.beginTransaction()
